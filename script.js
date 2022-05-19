4492a2b985b97461f9a192ade9a9ffc9d4a2436f
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-05-19 09:14:41 +0300

    minor fixes in JS file

--- script.js
+++ script.js
@@ -18,15 +18,38 @@ let wrapper = document.getElementById("game-wrapper")
 let displayText = document.getElementById("display-msg")
 
 
+function mouseDown(){
+    
+}
+
+function numConvert(number) {
+    if (number == 0){
+        return 
+    }
+}
+
 
 for (let i = 0; i < 15; i++){
         
     randNumber = (Math.floor(Math.random() * 4))
+    numberArray.push(randNumber)
 }
-numberArray.push(randNumber)
+
 
 function gameStart(){
         document.getElementById("display-msg").innerText = "Level " + level
+        
+        // for (let i in numberArray) {
+        //     if i 
+        // }
+        
+        
+        
+        setTimeout(function(id){
+            document.getElementsByTagName("p")[0].style.color = "#cccccc";
+        },3000);
+        
+        
         let green = document.getElementById("green");
         green.onclick = function(){
             greenSound.play()
@@ -37,6 +60,7 @@ function gameStart(){
         let red = document.getElementById("red");
         red.onclick = function(){
             redSound.play()
+            
             userInput.push("1")
         }
 
@@ -44,6 +68,7 @@ function gameStart(){
         let yellow = document.getElementById("yellow");
         yellow.onclick = function(){
             yellowSound.play()
+            yellow.style.backgroundColor
             userInput.push("2")
         }
 
