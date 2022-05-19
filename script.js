3a8ab8da774a7e4065adcdc40acafe79d5152550
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-05-19 04:57:27 +0300

    loaded sounds onto JS file

--- script.js
+++ script.js
@@ -11,21 +11,3 @@ yellowSound = new Audio('sounds/yellow.mp3');
 blueSound = new Audio('sounds/yellow.mp3');
 wrongSound = new Audio('sounds/wrong.mp3');
 
-
-window.addEventListener('keydown', function() {
-    let object = document.getElementById("green");
-    object.style.color = "white";
-
-})
-
-let red = document.getElementById("red");
-red.onclick = function(){
-    userInput.push("1")
-}
-
-let green = document.getElementById("green");
-green.onclick = function(){
-    userInput.push("0")
-}
-console.log("asij")
-console.log(userInput)
